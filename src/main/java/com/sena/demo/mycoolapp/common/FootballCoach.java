a168d902e7bb3d0161527263dd3215bd2fcd652a
Abdullah SEN <sen-abdullah@yandex.com>
2025-04-12 12:14:42 +0200

    Implement lazy initialization

--- src/main/java/com/sena/demo/mycoolapp/common/FootballCoach.java
+++ src/main/java/com/sena/demo/mycoolapp/common/FootballCoach.java
@@ -1,9 +1,15 @@
 package com.sena.demo.mycoolapp.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class FootballCoach implements Coach{
+
+    public FootballCoach() {
+        System.out.println("Constructor " + getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
